a8797d3c04d4510ab4925f03d469ca739366747b
zhmailikvadim <zhmailik_vadim@belsap.com>
2019-10-24 16:28:25 +0300

    change colors day

--- src/components/TableSap.js
+++ src/components/TableSap.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './TableSap.css'
 import SortImage from './SortImage';
-var ColorOver7 = "lightskyblue";
-var ColorFrom3To7 = "	lightyellow";
-var ColorOverTerm = "lightsalmon";
+var ColorOverTerm = "lightskyblue";
+var Color3To7 = "lightsalmon";
+var Color0To3 = "	lightyellow";
 export default props => (
      <table className="table">
      <caption>Аналитика продаж</caption>
@@ -20,7 +20,11 @@ export default props => (
             {props.data1.map((element, i) =>
                 <tr key = {i} onClick={props.onRowSelect.bind(null, element)}>
                     {Object.entries(element).map(([k,v]) =>
-                    (<td bgcolor={element.VBELN === 70206 ? ColorOver7 :ColorFrom3To7}>{v}</td>))}
+                    (<td bgcolor=
+                        {element.ZDAYS <= 0 ? ColorOverTerm //Оплата просрочена
+                        : element.ZDAYS <= 3 ? Color0To3 
+                        :element.ZDAYS <= 7 ? Color3To7
+                        :null}>{v}</td>))}
                 </tr>
             )}            
         </tbody>
